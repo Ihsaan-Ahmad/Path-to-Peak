606555f492189e0c3d04d5a913fcb25e8bf20993
Lindsay <lindsay.lbm@gmail.com>
2019-10-04 09:22:13 -0500

    converting ajax requests to functions based on filter need

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,7 @@
 /*Global Varibales=============================================================================================
 */ var parkInfo;
 var imageResponse;
+var stateSelected = "TX";
 
 //Start Function===============================================================================================
 $(document).ready(function () {
@@ -28,35 +29,56 @@ $(document).ready(function () {
 
   //Images fixed and working AJAX
   //Unsplash API Below: We are working on having the Unsplash API information incorporate photo based on image and location.
-  var userSearch = "" // What ever attribute they clicked on will go into the seach for our API Example"seach a park, click on park, image is random generated with that parks image"
-  var parksUrl =
-    "https://api.unsplash.com/photos/random/?client_id=de60a94209a9bef884f3a7ad3716dcb45770113151dea2b8e5aa7acc131efc54&query=" + userSearch; //<---here is where our users search generates image related from API
-  $.ajax({
-    method: "GET",
-    url: parksUrl
-  }).then(function (imgResponse) {
+  function unsplashAjaxRequest() {
+    var userSearch = "" // What ever attribute they clicked on will go into the seach for our API Example"seach a park, click on park, image is random generated with that parks image"
+    var parksUrl =
+      "https://api.unsplash.com/photos/random/?client_id=de60a94209a9bef884f3a7ad3716dcb45770113151dea2b8e5aa7acc131efc54&query=" + userSearch; //<---here is where our users search generates image related from API
+    $.ajax({
+      method: "GET",
+      url: parksUrl
+    }).then(unsplashAPICall);
+  }
+
+  function unsplashAPICall(imgResponse) {
     imageResponse = imgResponse;
     console.log(imageResponse);
-  });
-
+  }
+  // For state requests
+  function bothAjaxRequests() {
+    unsplashAjaxRequest;
+    NPSAjaxRequest;
+  }
+  $("#stateButton").on("click", bothAjaxRequests)
   //National Park API is below
-  var queryURL =
-    //Hide our key once ready to push! (12:57pm)
-    "https://developer.nps.gov/api/v1/parks?stateCode=TX&api_key=aYUUJ6WlFhXa4FxFlzz8RR712lrylzDsnov99dCe";
-  $.ajax({
-    method: "GET",
-    url: queryURL
-  }).then(function (response) {
+
+  function NPSAjaxRequest() {
+
+    var queryURL =
+      //Hide our key once ready to push! (12:57pm)
+      "https://developer.nps.gov/api/v1/parks?stateCode=" + stateSelected + "&api_key=aYUUJ6WlFhXa4FxFlzz8RR712lrylzDsnov99dCe";
+    $.ajax({
+      method: "GET",
+      url: queryURL
+    }).then(NPSAPICall);
+  }
+
+  function NPSAPICall(response) {
+    stateCardGenerator(response);
+    // modelGenerator(response)
+  }
+  function stateCardGenerator(response) {
     parkInfo = response;
     console.log(parkInfo);
 
     // Generating all park names and designations in the state
     var numParks = parkInfo.data.length
     console.log(numParks)
+    var stateContainer = $("<container id='stateCardContainer'>")
     for (i = 0; i < numParks; i++) {
       var parkName = parkInfo.data[i].fullName;
       userSearch = parkName;
       var parkDesignation = parkInfo.data[i].designation;
+      // Image from Unsplash
       var parkImage = imageResponse.urls.full;
       console.log(parkName);
       console.log(parkDesignation);
@@ -67,10 +89,6 @@ $(document).ready(function () {
       // Append card elements 
       stateParkCard.addClass("card")
       stateParkCard.attr("style", "width: 18rem;");
-
-      // Generating image from Unsplash
-      // var parkImage = 
-
       var stateImage = $("<img>").attr({ src: parkImage, class: "card-img-top", alt: "..." })
       stateParkCard.append(stateImage)
       var stateParkCardBody = $("<div class='card-body'>")
@@ -81,12 +99,11 @@ $(document).ready(function () {
       var stateParkCardP = $("<p class ='card-text'>");
       stateParkCardP.text(parkDesignation);
       stateParkCard.append(stateParkCardP);
-      stateParkCard.appendTo($("#card"));
+      stateContainer.append(stateParkCard);
+      stateContainer.appendTo($("#card"));
     }
     console.log(parkInfo)
-  });
-
-
+  }
 
   // HOME PAGE====================================================================================================
   // "take me home" button says take me home
