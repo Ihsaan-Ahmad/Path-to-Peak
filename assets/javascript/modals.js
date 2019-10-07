d3a18035e99a889344264c473d98af0f97cfbf22
Lindsay <lindsay.lbm@gmail.com>
2019-10-07 13:35:59 -0500

    wrapped geolocation in if statement in case lat and long not provided

--- assets/javascript/modals.js
+++ assets/javascript/modals.js
@@ -5,13 +5,19 @@ $(document).on("click", ".card-body", function () {
     // GRAB VALUE OF CARD
     var modalParkSelected = $(this).attr("data-value");
     var modalInfo = parkInfo.data.filter(item => item.parkCode === modalParkSelected)[0];
-    var latLong = modalInfo.latLong
-    console.log("LAT LONG: " + latLong);
-    latitude = modalInfo.latLong.split(",")[0].split(":")[1].trim();
-    longitude = modalInfo.latLong.split(",")[1].split(":")[1].trim();
-    console.log("LAT AND LONG: " + latLong);
-    geolocation(latitude, longitude)
-    console.log(modalInfo);
+
+    // Latitude and Longitude to city
+    if (modalInfo.latLong !== "") {
+        var latLong = modalInfo.latLong
+        console.log("LAT LONG: " + latLong);
+        latitude = modalInfo.latLong.split(",")[0].split(":")[1].trim();
+        longitude = modalInfo.latLong.split(",")[1].split(":")[1].trim();
+        console.log("LAT AND LONG: " + latLong);
+        geolocation(latitude, longitude)
+        console.log(modalInfo);
+    }
+
+    // Place info into modal
     var modalTitle = $(".modal-title")
     modalTitle.append(modalInfo.name + " " + modalInfo.designation);
     var modalBody = $(".modal-body");
